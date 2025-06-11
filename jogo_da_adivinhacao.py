08fd7be49ddf12b41026cf7656160c697284a27c
Pedro <pedro.dio.cruz@gmail>
2025-06-11 09:24:37 -0300

    corriginfo um erro

--- jogo_da_adivinhacao.py
+++ jogo_da_adivinhacao.py
@@ -5,12 +5,10 @@ print('---------JOGO DA ADIVINHACAO----------')
 
 numero_aleatorio = randint(1, 100)
 
-for i in range(10, 1, -1):
+for i in range(10, 0, -1):
     print(f'Voce tem {i} tentativas')
     numero_jogador = int(input("Digite um numero de 1 a 100: "))
-    if numero_jogador not in range(1, 100):
-        print("Digite um numero de 1 a 100")
-        
+
     if numero_jogador < numero_aleatorio:
         print('maior')
     elif numero_jogador > numero_aleatorio:
